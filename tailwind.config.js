204cf0f3b9b5efcdb6ced94e9c3f6545e63aa05a
Kodanda <kodandareddy21@gmail.com>
2022-08-21 21:18:28 +0530

    rode clone completed

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+       section1:"url('https://edge.rode.com/images/page/1/modules/5813/rode-rodecaster-pro-II-home-page-1920x700-rgb.jpg')",
+       section2:"url('https://edge.rode.com/images/page/1/modules/5873/rode-nth100-lifestyle-insitu-female-looking-down-grey-1920x700-rgb.jpg')",
+      section3:"url('https://edge.rode.com/images/page/1/modules/5914/rode-nt-usb-webheader-v7-1920x700-rgb.jpg')",
+      section4:"url('https://edge.rode.com/images/page/1/modules/1508/rode-podmic-webheader-1920x700-rgb%20(2).jpg')",
+      section5:"url('https://edge.rode.com/images/page/1/modules/314/WIGOII-WEBHEADER-02.jpg')",
+      section6:"url('https://edge.rode.com/images/page/1/modules/5303/rode-nt-usb-mini-webheader-1920x700-rgb%20(2).jpg')",
+      section7:"url('https://rode.com/assets/images%2Fpage%2F1%2Fmodules%2F5359%2Finterfaces-module-2022-1200x300-rgb.jpg')",
+
+      },
+      colors: {
+       cream:"#FFFFFF",
+       brown:"#6b6b6b",
+       gray:"#dedfe0",
+       gray50:"hsla(0,0%,100%,.5)",
+       liteGray:"#F8F9FA",
+       gold:"#AD9970",
+       darkGray:"#767676",
+       dark:"#484848"
+      }
+    },
+  },
+  plugins: [],
+}
